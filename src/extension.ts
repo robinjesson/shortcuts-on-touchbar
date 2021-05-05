e3276e494af74c6fa63966e12297df8cb2c063f5
Robin <robin.jesson@hotmail.fr>
2021-05-05 23:39:51 +0200

    Comment + correction

--- src/extension.ts
+++ src/extension.ts
@@ -7,58 +7,62 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	let squareBrackets = vscode.commands.registerCommand('extension.squareBrackets', () => {
-		addCharacter('[', ']');
+		wrapSelection('[', ']');
 	});
 
 	let curlyBrackets = vscode.commands.registerCommand('extension.curlyBrackets', () => {
-		addCharacter('{', '}');
+		wrapSelection('{', '}');
 	});
 
 	let stringInterpolation = vscode.commands.registerCommand('extension.stringInterpolation', () => {
-		addCharacter('\`', '\`');
+		wrapSelection('\`');
 	});
 
-	
+
 
 	context.subscriptions.push(
 		squareBrackets,
-		curlyBrackets);
+		curlyBrackets,
+		stringInterpolation);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
 
-function addCharacter(start: string, end?: string): void {
+/**
+ * Wraps specific characters arount selected text.
+ * @param first First character added to the selection. If the ```last``` character is not specified, then the ```start``` chararcter will be used.
+ * @param last Last character added to the selection.
+ */
+function wrapSelection(first: string, last?: string): void {
 	let editor = vscode.window.activeTextEditor;
 	if (editor) {
-		if(end) {
-			let selection = editor.selection;
-			
-			
-			let text = editor.document.getText(selection);
-			
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection, start + text + end);
-			});
+		let selection = editor.selection;
 
-			if(selection.isEmpty) {
-				// if there is nothing selected, replace the cursor between the two added characters
-				const position = editor.selection.end;
-				var newPosition = position.with(position.line, position.character + 1);
-        		var newSelection = new vscode.Selection(newPosition, newPosition);
-				editor.selection = newSelection;
-			}
-			else {
-				const position = editor.selection.end;
-				var newPosition = position.with(position.line, position.character + 2);
-        		var newSelection = new vscode.Selection(newPosition, newPosition);
-				editor.selection = newSelection;
-			}
+
+		let text = editor.document.getText(selection);
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(selection, first + text + (last ?? first));
+		});
+
+		if (selection.isEmpty) {
+			// if there is nothing selected, replace the cursor between the two added characters
+			const position = editor.selection.end;
+			var newPosition = position.with(position.line, position.character + 1);
+			var newSelection = new vscode.Selection(newPosition, newPosition);
+			editor.selection = newSelection;
+		}
+		else {
+			const position = editor.selection.end;
+			var newPosition = position.with(position.line, position.character + 2);
+			var newSelection = new vscode.Selection(newPosition, newPosition);
+			editor.selection = newSelection;
 		}
 	}
 	else {
-		vscode.window.showInformationMessage('There is no active text editor');
+		vscode.window.showInformationMessage('There is no active text editor.');
 	}
 }
 
- 
+
